cc6eea06be3901376122e300659be234d1eea2dc
AndreyJavaEd <scream123456@yandex.ru>
2023-08-25 17:37:52 +0300

    Применили многопоточность (одновременно отправляют запрос на сервер несколько клиентов). После этого сервер также многопоточно обрабатывает каждый запрос и отправляет ответ. Использовали отдельные классы для клиента и для сервера, которые имлементируют класс Thread и реализуют метод run().

--- network/src/main/java/edu/javacourse/net/Server.java
+++ network/src/main/java/edu/javacourse/net/Server.java
@@ -5,33 +5,53 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    public static void main(String[] args) throws IOException {
-        ServerSocket socket = new ServerSocket(25225); //Серверный сокет и ему передаем порт который он слушает
+    public static void main(String[] args) throws IOException, InterruptedException {
+        ServerSocket socket = new ServerSocket(25225, 2000); //Серверный сокет и ему передаем порт который он слушает
 
         System.out.println("Server started");
         while (true) {
             Socket client = socket.accept();//повисаем и ждем изменений
-            handleRequest(client);
+            new SimpleServer(client).start();
 
         }
     }
 
-    private static void handleRequest(Socket client) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
-        StringBuilder sb = new StringBuilder("Hello, ");
-        String userName = br.readLine();
-        System.out.println("Server got string: "+ userName);
 
+}
 
-        sb.append(userName);
-        bw.write(sb.toString());
-        bw.newLine();
-        bw.flush();
-
-        br.close();
-        bw.close();
-
-        client.close(); //поработали, сетевое соединение - прочитали, отправили и закрыли.
+class SimpleServer extends Thread {
+    private Socket client;
+    public SimpleServer(Socket client){
+        this.client=client;
     }
+    @Override
+    public void run() {
+        handleRequest();
+    }
+    private  void handleRequest() {
+
+        try {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+            StringBuilder sb = new StringBuilder("Hello, ");
+            String userName = br.readLine();
+            System.out.println("Server got string: " + userName);
+            Thread.sleep(2000);
+
+
+            sb.append(userName);
+            bw.write(sb.toString());
+            bw.newLine();
+            bw.flush();
+
+            br.close();
+            bw.close();
+
+            client.close();//поработали, сетевое соединение - прочитали, отправили и закрыли.
+        }catch (Exception ex){
+            ex.printStackTrace(System.out);
+        }
+    }
+
 }
