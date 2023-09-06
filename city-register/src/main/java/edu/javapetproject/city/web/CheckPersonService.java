ddf62771a9662c0710a0d83879124b70636cffc6
AndreyJavaEd <scream123456@yandex.ru>
2023-09-06 15:58:02 +0300

    Добавили библиотеки в pom.xml: org.glassfish.jersey.containers и org.glassfish.jersey.inject. В web.xml прописали правила, которые позволяют зарегистрировать классы. И написали класс сервиса CheckPersonService.java.

---
+++ city-register/src/main/java/edu/javapetproject/city/web/CheckPersonService.java
@@ -0,0 +1,13 @@
+package edu.javapetproject.city.web;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/check")
+public class CheckPersonService {
+    @GET
+    public String checkPerson(){
+        return "Simple String";
+    }
+
+}
