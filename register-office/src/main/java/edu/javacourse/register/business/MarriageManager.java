60b9a8b3f2c8b33ef36f81f0323b85267690f39e
AndreyJavaEd <scream123456@yandex.ru>
2023-11-03 00:42:04 +0300

    Создали MarriageController, который должен иметь доступ к Сервису, который называется MarriageManager.
    В MarriageManager.findMarriageCertificate() - метод возвращает нам ответ.
    Добавили в pom зависимость от slf4j-api и для вывода на экран подключили slf4j-simple.
    Создали Логер в Контроллере MarriageController и залогировали findMarriageCertificate(), также подключили логер в MarriageManager и залогировали метод также.
    Создали также метод в классе MarriageDao findMarriageCertificat(), который будет возвращать Сертификат.
    
    В классах MarriageController.java, MarriageManager.java, MarriageDao.java мы поставили сеттеры для объектов соответствующих классов.
    Подключили зависимости от spring-beans и spring-context для внедрения зависимостей в дальнейшем. Добавили файл конфигурации Спринга applicationContext.xml и в нем прописали тег context:component-scan и в нем пакет для сканирования.
    Далее проаннотировали Контроллер, Сервис и DAO аннотациями @Controller, @Service, @Component.
    
    Создали класс конфигурации MarriageConfig.java и аннотировали его аннотацией @Configuration.
    Создали файл проперти и в нем указали значение для поля test в классе MarriageDao, на полем поставили аннотацию @Value, а также загрузили файл register.properties в Спринг поставив над классом конфигурации MarriageConfig.java аннотацию @PropertySource и прописали в ее атрибутах путь к файлу проперти через classpath.
    
    Для проверки создали класс Starter в котором создали Спринг контекст и с помощью него получили бин Контроллера, далее на нем вызвали метод findMarriageCertificate().

--- register-office/src/main/java/edu/javacourse/register/business/MarriageManager.java
+++ register-office/src/main/java/edu/javacourse/register/business/MarriageManager.java
@@ -3,12 +3,27 @@ package edu.javacourse.register.business;
 import edu.javacourse.register.View.MarriageRequest;
 import edu.javacourse.register.View.MarriageResponse;
 import edu.javacourse.register.dao.MarriageDao;
+import edu.javacourse.register.dao.PersonDao;
+import edu.javacourse.register.domain.MarriageCertificate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MarriageManager {
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(MarriageManager.class);
     private MarriageDao marriageDao;
+    @Autowired
+    private PersonDao personDao;
+    @Autowired
+    public void setMarriageDao(MarriageDao marriageDao) {
+        this.marriageDao = marriageDao;
+    }
 
     public MarriageResponse findMarriageCertificate(MarriageRequest request){
-        throw new UnsupportedOperationException("Usupported");
+        LOGGER.info("findMarriageCertificate called");
+        MarriageCertificate certificate = marriageDao.findMarriageCertificate(request);
+        return new MarriageResponse();
     }
 }
--- register-office/src/main/java/edu/javacourse/register/dao/MarriageDao.java
+++ register-office/src/main/java/edu/javacourse/register/dao/MarriageDao.java
@@ -1,5 +1,27 @@
 package edu.javacourse.register.dao;
 
-public class MarriageDao {
+import edu.javacourse.register.View.MarriageRequest;
+import edu.javacourse.register.domain.MarriageCertificate;
+import jakarta.persistence.EntityManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
+public class MarriageDao {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MarriageDao.class);
+    private EntityManager entityManager;
+@Value("${test.value}")
+    private String test;
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+
+    public MarriageCertificate findMarriageCertificate(MarriageRequest request){
+        LOGGER.info("findMarriageCertificate called:{}",test);
+
+        return null;
+    }
 }
