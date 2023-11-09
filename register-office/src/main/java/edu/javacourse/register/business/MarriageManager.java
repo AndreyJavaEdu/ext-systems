c7a06df3d896063507b2a25987cd822817f7451c
AndreyJavaEd <scream123456@yandex.ru>
2023-11-09 04:53:45 +0300

    Подключили зависимость spring-data-jpa в pom.xml. Добавили схемы для работы со Spring Data в файл applicationContext.xml. Также в него добавили конфигурацию репозитория в теге <jpa:repositories>, в котором указали ссылку на entity manager и на transaction manager. Пометили класс MarriageDao аннотацией @Repository и сделали его как интерфейс. Изменили NamedQuery над классом Person для получения всех персон.
    В классе Сервиса MarriageManager в методе findMarriageCertificate происходит добавление двух персон, получаем сертификат в методе  getMarriageCertificate() в котором сертификат заполняется данными и персонами. Персоны мы создаем в методе getPerson() в зависимости от пола.
    Убрали инициализацию параметра для запроса на поиск персон в классе PersonDao.

--- register-office/src/main/java/edu/javacourse/register/business/MarriageManager.java
+++ register-office/src/main/java/edu/javacourse/register/business/MarriageManager.java
@@ -6,6 +6,7 @@ import edu.javacourse.register.dao.MarriageDao;
 import edu.javacourse.register.dao.PersonDao;
 import edu.javacourse.register.domain.MarriageCertificate;
 import edu.javacourse.register.domain.Person;
+import edu.javacourse.register.domain.PersonFemale;
 import edu.javacourse.register.domain.PersonMale;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,6 +17,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Service
 @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
@@ -26,21 +28,46 @@ public class MarriageManager {
     @Autowired
     private PersonDao personDao;
 
-@Transactional
-    public MarriageResponse findMarriageCertificate(MarriageRequest request){
+    @Transactional
+    public MarriageResponse findMarriageCertificate(MarriageRequest request) {
         LOGGER.info("findMarriageCertificate called");
-        MarriageCertificate certificate = marriageDao.findMarriageCertificate(request);
-        personDao.findPersons();
+//        MarriageCertificate certificate = marriageDao.findMarriageCertificate(request);
+//        personDao.findPersons();
 
-        Person m = new PersonMale();
-        m.setFirstName("Alex");
-        m.setLastName("Kirillov");
-        m.setPatronymic("Eduardovich");
-        m.setDateOfBirth(LocalDate.of(1991, 3, 12));
-        personDao.addPerson(m);
+        personDao.addPerson(getPerson(1));
+        personDao.addPerson(getPerson(2));
 
+        MarriageCertificate mc = getMarriageCertificate();
+        marriageDao.saveAndFlush(mc);
 
+        marriageDao.findAll();
 
         return new MarriageResponse();
+    }
+
+    private MarriageCertificate getMarriageCertificate() {
+        MarriageCertificate mc = new MarriageCertificate();
+        mc.setIssueDate(LocalDate.now());
+        mc.setNumber("12345");
+        mc.setActive(true);
+
+        List<Person> persons = personDao.findPersons();
+        for(Person person: persons){
+            if (person instanceof PersonMale){
+                mc.setHusband((PersonMale) person);
+            }else {
+                mc.setWife((PersonFemale) person);
+            }
+        }
+        return mc;
+    }
+
+    private Person getPerson(int sex) {
+        Person m = sex == 1 ? new PersonMale() : new PersonFemale();
+        m.setFirstName("1_" + sex);
+        m.setLastName("2_" + sex);
+        m.setPatronymic("3_" + sex);
+        m.setDateOfBirth(LocalDate.of(1991, 3, 12));
+        return m;
     }
 }
